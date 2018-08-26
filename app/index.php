b6be99c6323d76c5d525612e1ecd5bfa72a4abe6
hayashi taiki <hayashitaiki@team-lab.com>
2018-08-26 18:54:03 +0900

    クエリ追加

--- app/index.php
+++ app/index.php
@@ -1,18 +1,49 @@
-<?php echo '<p>PHP画面</p>'; ?>
 <?php
-echo '<p>Postgresへの接続テストコード</p>';
-$dsn = 'pgsql:dbname=postgres;host=skillup-php-step1_pgsql_1;port=5432';
-$user = 'postgres';
-$pass = 'example';
 try {
-    $dbh = new PDO($dsn, $user, $pass);
-    $sql = 'SELECT CURRENT_TIMESTAMP';
-    foreach ($dbh->query($sql) as $row) {
-        print "接続できている。<br/>現在時間は" . $row[0] . "です。\n";
-    }
-    $dbh = null;
-} catch (PDOException $e){
-    print('接続できていない。理由は以下<br/>[ERROR] ' . $e->getMessage() . "\n");
-    die();
+  // DBに接続する
+  $dbh = new PDO('mysql:host=mysql;dbname=test', 'root', 'password');
+
+  // ここでクエリ実行する
+  // queryメソッド(SELECT)
+  $query_result = $dbh->query('SELECT * FROM test_comments');
+  // prepareメソッド(INSERT)
+  $sth = $dbh->prepare('INSERT INTO test_comments (name, text) VALUES (?, ?)');
+  // prepareメソッド(SELECT)
+  $sth_select = $dbh->prepare('SELECT * FROM test_comments WHERE name = ?');
+
+  // DBを切断する
+  $dbh = null;
+} catch (PDOException $e) {
+  // 接続にエラーが発生した場合ここに入る
+  print "DB ERROR: " . $e->getMessage() . "<br/>";
+  die();
 }
 ?>
+
+======== queryメソッドを使用したSELECTの例 ========<br/>
+<?php
+  foreach($query_result as $row) {
+    print $row["name"] . ": " . $row["text"] . "<br/>";
+  }
+?>
+<br/>
+
+======== prepareメソッドを使用したINSERTの例 ========<br/>
+<?php
+  $name = "John";
+  $text = "All You Need Is Love!";
+  print "name:" . $name . ", text:" . $text . "で登録<br/>";
+  $sth->execute(array($name, $text));
+?>
+<br/>
+
+======== prepareメソッドを使用したSELECTの例 ========<br/>
+<?php
+  print $name . "の投稿を絞り込んで表示！<br/>";
+  $sth_select->execute(array($name));
+  $prepare_result = $sth_select->fetchAll();
+  foreach($prepare_result as $row) {
+    print $row["name"] . ": " . $row["text"] . "<br/>";
+  }
+?>
+<br/>
