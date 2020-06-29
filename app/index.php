239dad2b0d9b83f77dd464638aaf67f5565873d3
Rutile3 <Rutile3@outlook.jp>
2020-06-29 13:30:18 +0900

    ✨名前とコメントを送信できるよう修正

--- app/index.php
+++ app/index.php
@@ -7,19 +7,25 @@
   <body>
     <?php
       //commentがPOSTされているなら
-      if(isset($_POST["comment"])){
+      if(isset($_POST["name"]) && isset($_POST["comment"])){
         //エスケープしてから表示
+        $name = htmlspecialchars($_POST["name"]);
         $comment = htmlspecialchars($_POST["comment"]);
+        print("あなたの名前は「 ${name} 」で、");
         print("あなたのコメントは「 ${comment} 」です。");
       } else {
     ?>
-        <p>コメントしてください。</p>
         <form method="POST" action="index.php">
+          <p>名前を入力してください。</p>
+          <input name="name"" />
+          <br><br>
+
+          <p>コメントしてください。</p>
           <input name="comment" />
+          <br><br>
+
           <input type="submit" value="送信" />
         </form>
-    <?php
-      }
-    ?>
+    <?php } ?>
   </body>
 </html>
